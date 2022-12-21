583ba3f1cfeda0797dd034e34c45f7b5c12f0c72
fezeusabrina <audreyfezeu@gmail.com>
2022-12-21 15:50:57 +0100

    another commit 3

--- utils/authentification.js
+++ utils/authentification.js
@@ -1,10 +1,12 @@
 const jwt = require('jsonwebtoken');
 const {user : User} = require('../models/user');
 const bcrypt = require('bcrypt');
+const JWT_SECRET = '8b8cda03b2fb16506c66ecfcfc6bc8ad7ebb8a9e11e5df0a74343c8bf7e5c86b3670d25238d2b451c734f6d641398d57f79a7127123b810baa9fa87f3fc1bdfc'
+
 
 const generateTokenForUser = (user) => {
   const { password, ...rest } = user;
-  return jwt.sign({...rest._doc }, process.env.JWT_SECRET, {
+  return jwt.sign({...rest._doc }, JWT_SECRET, {
     expiresIn: '1h',
   });
 };
--- utils/tokenVerification.js
+++ utils/tokenVerification.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const JWT_SECRET = '8b8cda03b2fb16506c66ecfcfc6bc8ad7ebb8a9e11e5df0a74343c8bf7e5c86b3670d25238d2b451c734f6d641398d57f79a7127123b810baa9fa87f3fc1bdfc'
+
 
 /**
  *
@@ -9,7 +11,7 @@ const jwt = require('jsonwebtoken');
 module.exports.tokenVerification = async (token) => {
   return new Promise((resolve, reject) => {
     if (token) {
-      const isValidateToken = jwt.verify(token, process.env.JWT_SECRET);
+      const isValidateToken = jwt.verify(token, JWT_SECRET);
       if (isValidateToken) {
         resolve({ isValidateToken });
       } else {
